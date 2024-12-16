602aafb76dc03ddcf082ffd51fa314d291eb891b
Namazbek Bekzhanov <119422414+mrnamazbek@users.noreply.github.com>
2024-12-16 11:39:14 +0500

    hided tokens twitter_etl.py

--- twitter_etl.py
+++ twitter_etl.py
@@ -4,18 +4,18 @@ import json
 from datetime import datetime
 import s3fs
 
-#1: H937Qxph85S6DkicHCR8E2ZEE
-#2: 5GBbBVeYYngwhH7E2eLH6uiqY9d8D3k9OB8x3zFB29p1reN0tX
-#3: bearer toke: AAAAAAAAAAAAAAAAAAAAAD0wxQEAAAAAKXyFCAp1EWIgUfcQVworDt7ovmc%3Dlbz24pBSnct85npRlcZbFrp4vRccw7faC40xu8A9e06Vn7MR7T
-#4: access token: 1863259480437235712-c3z1fDyNR5vwCLaZQ0PQ1hrCaJHtqA
-#5: access secret: nFbnkiE9b9ywfmRshLjfUDW0VUObFPhIWkyJehnNwJasV
+#1: ***********
+#2: *************************
+#3: bearer toke: ******************************************************
+#4: access token: ********************************************
+#5: access secret: ********************************************
 
 import tweepy
 import pandas as pd
 
 
 def run_twitter_etl():
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAD0wxQEAAAAAKXyFCAp1EWIgUfcQVworDt7ovmc%3Dlbz24pBSnct85npRlcZbFrp4vRccw7faC40xu8A9e06Vn7MR7T"
+    bearer_token = "your_bearer_token"
 
     # Create a client connection
     client = tweepy.Client(bearer_token=bearer_token)
